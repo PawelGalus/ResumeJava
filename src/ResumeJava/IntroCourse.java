0b5e167f924ab4654f309964c9aa179da8d2fa0f
PawelGalus <Pawe Galus@AsusROGStrix>
2022-09-12 12:36:44 +0200

    Rename class Add package

---
+++ src/ResumeJava/IntroCourse.java
@@ -0,0 +1,12 @@
+package ResumeJava;
+
+/**
+ * Course: Intro to Java Programming - Course for Absolute Beginners.
+ */
+public class IntroCourse {
+
+	public static void main(String[] args) {
+
+	}
+
+}
